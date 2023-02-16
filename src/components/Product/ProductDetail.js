e71ed79bef67d99f22d9206334f3061cef83d921
minhnqse161262 <minhnqse161262@fpt.edu.vn>
2023-02-16 08:32:32 +0700

    up_proDetail

--- src/components/Product/ProductDetail.js
+++ src/components/Product/ProductDetail.js
@@ -1,7 +1,52 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import * as Config from "../../utils/Config";
+import axios from "axios";
+import { Container,Row,Col,Card,Button } from 'react-materialize'
 export default function ProductDetail() {
+    const Productid = useParams();
+    const [productlist, setProductList] = useState([])
+    console.log(Productid);
+
+    function getData() {
+        axios.get(`${Config.API_URL}/Product/` + Productid.id)
+            .then(response => response.data)
+            .then((data) => {
+              setProductList(data)
+            });
+        console.log(productlist);
+    }
+
+    useEffect(()=>{
+        getData();
+    },[])
+
     return (
-        <div>
-            <h1>Product Detail</h1>
-        </div>
-    )
+      <div>
+      <Container>
+          <br/>
+          <Row >
+              <Col s={12} m={6} l={4}  >
+                  <Card>
+                      <div className='Product-info'>
+                      <img src={productlist.Image} />
+                      
+                      <div className='Product-item-detail'>
+                      <strong className='ProductName'>{productlist.ProductName}</strong>
+                      <div className='Product-item-price'>
+                          <div className='Price-money'>
+                              <span className='label'>Price</span>
+                              <span className='Price'>{productlist.Price} VNĐ</span>
+                          </div>
+                      </div>
+                      <p className='placeholder'></p>
+                      </div>
+                      </div>
+                  </Card>
+              </Col>
+          </Row>
+      </Container>
+  </div>
+
+    );
 }
